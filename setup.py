d0ae6457d647dd0bd0e3891124c5698bcfcc5534
Will Gittoes <willgittoes@gmail.com>
2017-07-01 18:10:21 +1000

    lol forgot these

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+
+setup(name='dcgan', version='ayy lmao', description='ayy lmao', author='A bunch of people',
+      url='', packages=['dcgan'],
+      dependency_links=['git+https://willberg@bitbucket.org/willberg/ewlol.git'])
